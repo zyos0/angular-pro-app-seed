3b70ea3a65b690b481922724b6cd6f1de00cc817
Roberto Malaver <robertomalaver7@gmail.com>
2018-07-13 18:08:52 -0500

    Meal component also  ngif else used

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -4,10 +4,12 @@ import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
 import {RouterModule, Routes} from "@angular/router";
 import {SharedModule} from "../shared/shared.module";
+import {MealComponent} from "./containers/meal/meal.component";
 
 
 export const ROUTES: Routes = [
-  {path: '', component: MealsComponent}
+  {path: '', component: MealsComponent},
+  {path: 'new', component: MealComponent},
 ];
 
 @NgModule({
@@ -17,7 +19,7 @@ export const ROUTES: Routes = [
     RouterModule.forChild(ROUTES),
     SharedModule
   ],
-  declarations: [MealsComponent],
+  declarations: [MealsComponent, MealComponent],
   providers: []
 })
 export class MealsModule {
